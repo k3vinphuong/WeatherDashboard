8fb35d7b05ebcd137785f8087adba25cf4440335
k3vinphuong <161548597+k3vinphuong@users.noreply.github.com>
2025-08-14 13:59:16 +1000

    started on some JS and started CSS

---
+++ app.js
@@ -0,0 +1,20 @@
+const apiKey = "2f7dd0fb87c2a3c1314c918804c19256";
+const weatherURL = "https://api.openweathermap.org/data/2.5/weather";
+const forecastURL = "https://api.openweathermap.org/data/2.5/forecast";
+
+const cityInput = document.getElementById("city-input");
+const searchBtn = document.getElementById("search-btn");
+const mainCity = document.querySelector(".mainweather h1");
+const mainTemp = document.querySelector(".mainweather .temp");
+const mainIcon = document.querySelector(".mainweather img");
+const forecastContainer = document.querySelector(".weekforecast");
+
+const sunTimes = document.getElementById("sun-time");
+const windInfo = document.getElementById("wind-info");
+const humidityInfo = document.getElementById("humidity-info");
+const precip = document.getElementById("rain-level");
+
+function formatTime(timestamp) {
+    const date = new Date(timestamp * 1000);
+    return date.toLocaleDateString([], { hour: '2-digit', minute: '2-digit' });
+}
